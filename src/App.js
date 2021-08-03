bb22b2998c0753ef71003b7f49d5588d35a29cfa
Matthew Duffy <51209857+mattduff707@users.noreply.github.com>
2021-08-03 12:28:38 -0500

    Create app skeleton (#1)
    * add: Created app skeleton
    
    Installed packages:
     - styled-components
     - react-router-dom
     - normalize.css
    
    Created layout components Header and Main. Added routing for Main
    section. Also added styled-components global styles. Added Links for
    fonts in index.html head. Reset default styles using normalize.css. And
    added components and layout folders
    
    * Add: normalize in global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+// eslint-disable-next-line quotes
 import React from 'react';
+import { normalize } from 'styled-normalize';
+import { BrowserRouter as Router } from 'react-router-dom';
+import styled, { createGlobalStyle } from 'styled-components';
+import Header from './layout/Header';
+import Main from './layout/Main';
 
+const GlobalStyle = createGlobalStyle`
+${normalize}
+html {
+--font-family-primary: 'Bitter', serif;
+--font-family-secondary: 'Montserrat', sans-serif;
+
+}
+body, #root, html {
+  height: 100%;
+}
+
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+`;
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </Router>
   );
 }
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+`;
 
 export default App;
